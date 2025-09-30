8d9b7a505621af90b3cba5a629758238b9b58fd3
Forest van Eeden <forestvaneeden@icloud.com>
2025-09-29 20:53:45 -0400

    Update constants.js

--- Boem/conservation/natural_conservation/organic_conservation/organic_beholding/organic_preservation/organic_network/2-fime_growth/Solutions/Interpanel/organic/constants.js
+++ Boem/conservation/natural_conservation/organic_conservation/organic_beholding/organic_preservation/organic_network/2-fime_growth/Solutions/Interpanel/organic/constants.js
@@ -1,1 +1,14 @@
+// Global Organic Parameters
+export const COLORS = {
+  background: "#000000",   // dark safety
+  outline: "#e66c02",      // organic orange
+  highlight: "#00ee82"     // bioluminescent green
+};
 
+export const EULER = 2.718281828;  // Euler’s constant
+
+// Useful primes (expandable)
+export const PRIMES = [
+  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
+  37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79
+];
