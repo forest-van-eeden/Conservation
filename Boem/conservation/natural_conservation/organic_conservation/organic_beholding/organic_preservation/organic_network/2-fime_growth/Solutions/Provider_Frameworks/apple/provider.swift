6e63f95298daf4fdca4fda7b8786d3c11899a221
Forest van Eeden <forestvaneeden@icloud.com>
2026-02-09 14:40:35 -0800

    Update provider.swift

--- Boem/conservation/natural_conservation/organic_conservation/organic_beholding/organic_preservation/organic_network/2-fime_growth/Solutions/Provider_Frameworks/apple/provider.swift
+++ Boem/conservation/natural_conservation/organic_conservation/organic_beholding/organic_preservation/organic_network/2-fime_growth/Solutions/Provider_Frameworks/apple/provider.swift
@@ -29,10 +29,22 @@ func provider() {
 		for space in spaces {
 			
 				let material = space.trimmingCharacters(in: .whitespaces)
-				
-				if material.isEmpty { continue }
+
+				// Need : what are .trimmingCharacter and .whitespaces
 			
-				//if material.hasPrefix	
+				if material.isEmpty { continue }
+
+				// Learning Code Words
+				// if material.hasNoPrefix { make into code_word }
+				// code_word is like a function notation
+				// if code_word { a step can be underneath }
+				
+				// if material.hasPrefix("•") { make into step }
+				// if step { this is a task to do as if inside of the code_word like it was a function }
+				// steps are to be done in order of appearance
+
+				// file.interation is a code_word
+				// the step is for the provider to then go to a file that is input that way in an .interaction document
 		}
 		
 	}
