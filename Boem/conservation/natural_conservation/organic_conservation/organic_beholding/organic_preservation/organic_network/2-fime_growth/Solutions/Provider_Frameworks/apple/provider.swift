5315a2ff0e3e335ee5171299731fe4199cacc23f
Forest van Eeden <forestvaneeden@icloud.com>
2026-02-09 15:20:06 -0800

    Update provider.swift

--- Boem/conservation/natural_conservation/organic_conservation/organic_beholding/organic_preservation/organic_network/2-fime_growth/Solutions/Provider_Frameworks/apple/provider.swift
+++ Boem/conservation/natural_conservation/organic_conservation/organic_beholding/organic_preservation/organic_network/2-fime_growth/Solutions/Provider_Frameworks/apple/provider.swift
@@ -3,20 +3,20 @@ import Foundation
 func provider() {
 	
 	// Call File Manager
-	let file_manager = FileManager.default
+	let looking = FileManager.default
 	
 	// Looking Path
-	let current_path = file_manager.currentDirectoryPath
+	let current_directory = looking.currentDirectoryPath
 	
 	// Name Format File
 	let interaction_format = "format.interaction"
 	
 	// Format Path
-	let format_path = "\(current_path)/Interaction/\(interaction_format)"
+	let format_path = "\(current_directory)/Interaction/\(interaction_format)"
 	
 	print ("Looking for format.interaction at \(format_path)...")
 	
-	if file_manager.fileExists(atPath: format_path) {
+	if looking.fileExists(atPath: format_path) {
 		
 		print ("Located interaction.format")
 
@@ -27,23 +27,31 @@ func provider() {
 		let spaces = format.components(separatedBy: .newlines)
 		
 		for space in spaces {
-			
-				let material = space.trimmingCharacters(in: .whitespaces)
 
-				// Need : what are .trimmingCharacter and .whitespaces
-			
+				// Defining Material Space
+				let material = space.trimmingCharacters(in: .whitespaces)			
 				if material.isEmpty { continue }
 
-				// Learning Code Words
+				// Learning Code Words Space
+				var code_word: String = ""
 				// if material.hasNoPrefix { make into code_word }
 				// code_word is like a function notation
 				// if code_word { a step can be underneath }
-				
-				// if material.hasPrefix("•") { make into step }
+
+				// Learning Steps Space
+				if material.hasPrefix("•") { 
+					let step = material.replacingOccurrences(of: "•", with: "").trimmingCharacters(in: .whitespaces)
+				}
 				// if step { this is a task to do as if inside of the code_word like it was a function }
 				// steps are to be done in order of appearance
-
+			
+				// Learning File Extensions Space
 				// file.interation is a code_word
+				if code_word.contains(".interaction") {
+					print("Learning \(code_word)")
+				} else {
+					code_word = material
+				}
 				// the step is for the provider to then go to a file that is input that way in an .interaction document
 		}
 		
